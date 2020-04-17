ba0440c6dcebbf5e6af593d5b039743f4ecbaec4
Alex Kay <alex110504@gmail.com>
2020-04-17 10:33:19 -0700

    Trying to outline app deployment stages: bare-bone vs incremental

--- deployment.sh
+++ deployment.sh
@@ -1,3 +1,4 @@
+# Install Application
 cp .env.test .env
 
 # prep variables for the .env file
@@ -9,12 +10,13 @@ echo "DATABASE_URL=\"$DATABASE_CONNECTOR\"" >> .env
 
 composer install
 
-rclone --config=/home/$INSTANCE_USER/datastore.conf copy $DATA_REMOTE:$BUCKET_NAME/data/source /var/www/html/data/source
-
 bin/console doctrine:database:create
-bin/console doctrine:migrations:migrate --no-interaction 
+bin/console doctrine:migrations:migrate --no-interaction
 bin/console doctrine:fixtures:load --group=Instruments --no-interaction
+rclone --config=/home/$INSTANCE_USER/datastore.conf copy $DATA_REMOTE:$BUCKET_NAME/data/source /var/www/html/data/source
 bin/console doctrine:fixtures:load --group=OHLCV --no-interaction --append
 
+
+# Test Installation
 bin/phpunit tests/Service/Exchange
 bin/phpunit tests/Service/PriceHistory/OHLCV
