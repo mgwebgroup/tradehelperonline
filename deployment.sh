24377baab69309ec063ba6ec2944dc531c556063
Alex Kay <alex110504@gmail.com>
2020-04-21 13:03:51 -0700

    Modified deployment script

--- deployment.sh
+++ deployment.sh
@@ -1,22 +1,33 @@
-# Install Application
-cp .env.test .env
+while (( "$#" > 0 )) ; do
+  case $1 in
+    bare)
+      echo 'will perform bare install'
+      cp .env.test .env
+      # prep variables for the .env file
+      echo "APP_ENV=$APP_ENV" >> .env
+      echo "DATABASE_URL=\"$DATABASE_CONNECTOR\"" >> .env
+      composer install
+      ;;
+    fixtures)
+      echo 'will add fixtures'
+      bin/console doctrine:database:create
+      bin/console doctrine:migrations:migrate --no-interaction
+      bin/console doctrine:fixtures:load --group=Instruments --no-interaction
 
-# prep variables for the .env file
-echo "APP_ENV=$APP_ENV" >> .env
-echo "DATABASE_URL=\"$DATABASE_CONNECTOR\"" >> .env
+      rclone --config=/home/$INSTANCE_USER/datastore.conf copy $DATA_REMOTE:$BUCKET_NAME/data/source /var/www/html/data/source
+      bin/console doctrine:fixtures:load --group=OHLCV --no-interaction --append
+      ;;
+    tests)
+      echo 'will run unit tests'
+      bin/phpunit tests/Service/Exchange
+      bin/phpunit tests/Service/PriceHistory/OHLCV
+      ;;
+    *) echo "invalid directive $1"
+      ;;
+  esac
+  shift
+done
 
-# Include bash scripts to formulate y_universe and lists for nyse, amex, and nasdaq
-# ...
+## Include bash scripts to formulate y_universe and lists for nyse, amex, and nasdaq
+## ...
 
-composer install
-
-bin/console doctrine:database:create
-bin/console doctrine:migrations:migrate --no-interaction
-bin/console doctrine:fixtures:load --group=Instruments --no-interaction
-rclone --config=/home/$INSTANCE_USER/datastore.conf copy $DATA_REMOTE:$BUCKET_NAME/data/source /var/www/html/data/source
-bin/console doctrine:fixtures:load --group=OHLCV --no-interaction --append
-
-
-# Test Installation
-bin/phpunit tests/Service/Exchange
-bin/phpunit tests/Service/PriceHistory/OHLCV
