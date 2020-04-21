298dcc743eb05453412115a9a65405ad6a3edcc1
Alex Kay <alex110504@gmail.com>
2020-04-21 13:21:08 -0700

    Separated rclone into data arg on deployment script

--- deployment.sh
+++ deployment.sh
@@ -8,13 +8,16 @@ while (( "$#" > 0 )) ; do
       echo "DATABASE_URL=\"$DATABASE_CONNECTOR\"" >> .env
       composer install
       ;;
+    data)
+      echo 'will copy application data from aws'
+      rclone --config=/home/$INSTANCE_USER/datastore.conf copy $DATA_REMOTE:$BUCKET_NAME/data/source /var/www/html/data/source
+      ;;
     fixtures)
       echo 'will add fixtures'
       bin/console doctrine:database:create
       bin/console doctrine:migrations:migrate --no-interaction
       bin/console doctrine:fixtures:load --group=Instruments --no-interaction
 
-      rclone --config=/home/$INSTANCE_USER/datastore.conf copy $DATA_REMOTE:$BUCKET_NAME/data/source /var/www/html/data/source
       bin/console doctrine:fixtures:load --group=OHLCV --no-interaction --append
       ;;
     tests)
