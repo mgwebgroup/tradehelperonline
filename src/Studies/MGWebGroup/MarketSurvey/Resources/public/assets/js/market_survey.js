4e74f0e26b564052d41c8d9a70c804190250322d
Alex Kay <alex110504@gmail.com>
2020-12-24 13:36:57 -0700

    Addes sortability to the sectors table

--- src/Studies/MGWebGroup/MarketSurvey/Resources/public/assets/js/market_survey.js
+++ src/Studies/MGWebGroup/MarketSurvey/Resources/public/assets/js/market_survey.js
@@ -4,13 +4,29 @@ import '../scss/study.scss';
 
 window.onload = function()
 {
+    // AS List symbols are clickable
     $('.symbol').click(function() {
         var date = $(this).attr('date');
         var symbol = $(this).attr('symbol');
 
-        // console.log('date='+date+' symbol='+symbol);
         fetchChartWindow(symbol, date);
     });
+
+
+    // make sectors table sortable
+    const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
+    const comparer = (idx, asc) => (a, b) => ((v1, v2) =>
+            v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
+    )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+
+    document.querySelectorAll('.sectors .table th').forEach(th => th.addEventListener('click', (() => {
+        const table = th.closest('table');
+        const tbody = table.querySelector('tbody');
+        Array.from(tbody.querySelectorAll('tr:nth-child(-n+11)')).sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc)).forEach(tr => tbody.appendChild(tr) );
+
+        // move last 2 summary rows back to bottom
+        tbody.querySelectorAll('tr:nth-child(-n+2)').forEach(tr => tbody.appendChild(tr));
+    })));
 }
 
 function fetchChartWindow(symbol, date)
