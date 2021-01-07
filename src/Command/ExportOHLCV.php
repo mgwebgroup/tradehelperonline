70c87cf2e2e86684c9bfa5614298e4bd10afd3ec
Alex Kay <alex110504@gmail.com>
2021-01-07 14:30:18 -0700

    Refactored th:price:export command

--- src/Command/ExportOHLCV.php
+++ src/Command/ExportOHLCV.php
@@ -1,4 +1,6 @@
-<?php /** @noinspection DuplicatedCode */
+<?php
+
+/** @noinspection DuplicatedCode */
 
 /**
  * This file is part of the Trade Helper package.
@@ -13,6 +15,10 @@ namespace App\Command;
 
 use App\Entity\Instrument;
 use App\Entity\OHLCV\History;
+use App\Exception\PriceHistoryException;
+use DateTime;
+use Doctrine\ORM\EntityManager;
+use Exception;
 use League\Csv\Reader;
 use League\Csv\Statement;
 use League\Csv\Writer;
@@ -22,21 +28,17 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Exception\IOException;
 use Symfony\Component\Filesystem\Filesystem;
 use App\Service\UtilityServices;
 
 class ExportOHLCV extends Command
 {
-    const LIST_PATH = 'data/source/x_universe.csv';
-
-    const EXPORT_PATH = 'data/source/ohlcv';
-
-    const INTERVAL_DAILY = 'P1D';
-
-    const START_DATE = '2011-01-01';
+    public $listPath = 'data/source/x_universe.csv';
+    public $exportPath = 'data/source/ohlcv';
 
     /**
-     * @var Doctrine\ORM\EntityManager
+     * @var EntityManager
      */
     protected $em;
 
@@ -46,7 +48,7 @@ class ExportOHLCV extends Command
     protected $filesystem;
 
     /**
-     * @var \App\Service\UtilityServices
+     * @var UtilityServices
      */
     protected $utilities;
 
@@ -68,16 +70,20 @@ class ExportOHLCV extends Command
     protected $chunk;
 
     /**
-     * @var string
+     * @var Instrument
      */
-    protected $symbol;
+    protected $instrument;
+
+    /**
+     * @var Reader
+     */
+    protected $csvReader;
 
     public function __construct(
         Filesystem $filesystem,
         RegistryInterface $doctrine,
         UtilityServices $utilities
-    )
-    {
+    ) {
         $this->filesystem = $filesystem;
         $this->em = $doctrine;
         $this->utilities = $utilities;
@@ -91,151 +97,199 @@ class ExportOHLCV extends Command
 
         $this->setDescription('Exports prices stored on OHLCV format from database into csv files');
 
-        $this->setHelp(
-            <<<'EOT'
-In the first form uses a csv file with header to define list of symbols to work on. You can export history for all or
-several consecutive symbols listed in the y_universe file. To complete export of several symbols specify --offset 
-and --chunk options. Header starts with offset=0. To export all symbols don't specify these options. Example can be:
+        $this->setHelp("Exports OHLCV price data stored in database into csv file(s). The command can work either with a list of symbols (specified as a .csv file), or with one symbol (specified as the --symbol option). When working with the list, --offset and --chunk options can point to only parts of the list. Header in the file must be present and starts with offset = 0. Only one column is necessary to be present in header: Symbol (case sensitive). You may have other columns as well. Order of columns is not important. Default path and file name: {$this->listPath}.\nPrice data will be exported as .csv files following this convention: SYMBOL_d|w|m|y.csv.\nIf you omit the --provider option, price records where provider field is set to null will be exported.\nIf there is an existing file with already exported prices, it will be backed up the with .bak extension.");
 
-bin/console price:export --provider=YAHOO [data/source/x_universe.csv] [data/source/ohlcv]
-
-In the second form, command will use --symbol option to look for a specific one symbol in the price history database.
- The symbol has to be listed in the y_universe, or other file if it replaces the y_universe.
-For example, to export daily history for Facebook, you can use this:
-
-bin/console price:export --symbol=FB --provider=YAHOO [data/source/ohlcv] 
-
-Take note of the --provider option. If you don't specify a provider, i.e. omit this option, ALL price records (for 
-default interval daily) will be exported. This way you may end up for multiple records for same date, because they are 
-stored for multiple providers in db. 
-
-If there is an existing file with .csv prices found in the (default) export directory, it will be backed up with .bak 
- extension.
-EOT
+        $this->addUsage(
+            '[--from-date] [--to-date] [--offset=int] [--chunk=int] [--interval=P1D] [--provider=YAHOO]
+          --list=data/source/x_universe.csv data/source/ohlcv'
+        );
+        $this->addUsage(
+            '[--from-date] [--to-date] [--interval=P1D] [--provider=YAHOO] --symbol=FB [data/source/ohlcv]'
         );
 
-        $this->addUsage('[-v] [--from-date] [--to-date] [--offset=int] [--chunk=int] [--interval=P1D] --provider=YAHOO [data/source/x_universe.csv] [data/source/ohlcv]');
-        $this->addUsage('[-v] [--from-date] [--to-date] [--offset=int] [--chunk=int] [--interval=P1D] --provider=YAHOO --symbol=FB [data/source/ohlcv]');
-
-        $this->addArgument('input_path', InputArgument::OPTIONAL, 'Path/to/file.csv with list of symbols to work on', self::LIST_PATH);
-        $this->addArgument('export_path', InputArgument::OPTIONAL, 'Path/to/directory for csv files to export', self::EXPORT_PATH);
-        $this->addOption('interval', null, InputOption::VALUE_REQUIRED, 'Time interval to export OHLCV data for', self::INTERVAL_DAILY);
-        $this->addOption('from-date', null, InputOption::VALUE_REQUIRED, 'Start date of stored history', self::START_DATE);
+        $this->addArgument(
+            'export-path',
+            InputArgument::OPTIONAL,
+            'Path/to/directory for price files to export',
+            $this->exportPath
+        );
+        $this->addOption(
+            'list-file',
+            null,
+            InputOption::VALUE_REQUIRED,
+            'Path/to/file.csv with list of symbols to work on',
+            $this->listPath
+        );
+        $this->addOption(
+            'interval',
+            null,
+            InputOption::VALUE_REQUIRED,
+            'Time interval to export OHLCV data for (daily, weekly, monthly, quarterly, yearly)',
+            History::INTERVAL_DAILY
+        );
+        $this->addOption(
+            'from-date',
+            null,
+            InputOption::VALUE_REQUIRED,
+            'Start date of stored history'
+        );
         $this->addOption('to-date', null, InputOption::VALUE_REQUIRED, 'End date of stored history');
         $this->addOption('chunk', null, InputOption::VALUE_REQUIRED, 'Number of records to process in one chunk');
-        $this->addOption('offset', null, InputOption::VALUE_REQUIRED, 'Starting offset in y_universe file. Header has offset=0');
-        $this->addOption('provider', null, InputOption::VALUE_REQUIRED, 'Name of Price Provider. Must match that in PriceProvider class');
+        $this->addOption(
+            'offset',
+            null,
+            InputOption::VALUE_REQUIRED,
+            'Starting offset in y_universe file. Header has offset=0'
+        );
+        $this->addOption(
+            'provider',
+            null,
+            InputOption::VALUE_REQUIRED,
+            'Name of Price Provider. Must match that in PriceProvider class'
+        );
         $this->addOption('symbol', null, InputOption::VALUE_REQUIRED, 'Symbol to export');
     }
 
     protected function initialize(InputInterface $input, OutputInterface $output)
     {
-        if ($provider = $input->getOption('provider')) {
-            $this->provider = $provider;
-        } else {
-            $this->provider = null;
-        }
-
-        if ($input->getOption('offset')) {
-            $this->offset = $input->getOption('offset');
-        } else {
-            $this->offset = 0;
-        }
-
-        if ($input->getOption('chunk')) {
-            $this->chunk = $input->getOption('chunk');
-        } else {
-            $this->chunk = -1;
-        }
-
         if ($symbol = $input->getOption('symbol')) {
-            $this->symbol = $symbol;
-        } else {
-            $this->symbol = null;
+            $this->instrument = $this->em->getRepository(Instrument::class)->findOneBySymbol($symbol);
+            if (empty($this->instrument)) {
+                $output->writeln(sprintf('<error>ERROR: </error>Instrument for symbol %s is not imported', $symbol));
+                exit(1);
+            }
+        }
+
+        if ($listFile = $input->getOption('list-file')) {
+            if (false === $this->filesystem->exists($listFile)) {
+                $output->writeln(sprintf(
+                    '<error>ERROR: </error>File for the symbols list could not be found. Looked for: %s',
+                    $listFile
+                ));
+                exit(1);
+            }
+
+            $this->csvReader = Reader::createFromPath($listFile);
+        }
+
+        $exportPath = $input->getArgument('export-path');
+        if (false === $this->filesystem->exists($exportPath)) {
+            $output->writeln(sprintf('Could not find export path %s', $exportPath));
+            exit(1);
+        }
+
+        try {
+            $tempFile = $this->filesystem->tempnam($exportPath, 'mgweb');
+            $this->filesystem->remove($tempFile);
+        } catch (IOException $e) {
+            $output->writeln(sprintf('<error>ERROR: </error>%s', $e->getMessage()));
+            exit(1);
         }
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): ?int
     {
         $this->utilities->pronounceStart($this, $output);
 
-        $inputFile = $input->getArgument('input_path');
-        $exportPath = $input->getArgument('export_path');
-        $interval = $input->getOption('interval');
-
-        $fromDate = new \DateTime($input->getOption('from-date'));
-        $toDate = $input->getOption('to-date')? new \DateTime($input->getOption('to-date')) : null;
-
-        switch ($interval) {
-            case 'P1D':
-                $period = 'd';
-                break;
-            case 'P1W':
-                $period = 'w';
-                break;
-            case 'P1M':
-                $period = 'm';
-                break;
-            default:
-                throw new \Exception(sprintf('Period %s is not serviced', $interval));
+        try {
+            $fromDate = $input->getOption('from-date') ? new DateTime($input->getOption('from-date')) : null;
+            $toDate = $input->getOption('to-date') ? new DateTime($input->getOption('to-date')) : null;
+        } catch (Exception $e) {
+            $output->writeln(sprintf('<error>ERROR: </error>%s', $e->getMessage()));
+            exit(1);
         }
 
-        $repository = $this->em->getRepository(Instrument::class);
+        try {
+            $intervalRaw = strtolower($input->getOption('interval'));
+            $interval = History::getOHLCVInterval($intervalRaw);
+        } catch (PriceHistoryException $e) {
+            $output->writeln(sprintf('<error>ERROR: </error>%s', $e->getMessage()));
+            exit(1);
+        }
 
-        $csv = Reader::createFromPath($inputFile, 'r');
-        $csv->setHeaderOffset(0);
-        $statement = new Statement();
-        if ($this->symbol) {
-            $statement = $statement->where(function($v) { return $v['Symbol'] == $this->symbol; });
+        $provider = $input->getOption('provider');
+        $exportPath = $input->getArgument('export-path');
+        $period = substr($intervalRaw, 0, 1);
+
+        if ($this->instrument) {
+            $records[] = ['Symbol' => $this->instrument->getSymbol()];
         } else {
-            if ($this->offset > 0) {
-                $statement = $statement->offset($this->offset - 1);
+            $this->csvReader->setHeaderOffset(0);
+            $statement = new Statement();
+            $offset = $input->getOption('offset');
+            $chunk = $input->getOption('chunk');
+            if ($offset > 0) {
+                $statement = $statement->offset($offset - 1);
             }
-            if ($this->chunk > 0) {
-                $statement = $statement->limit($this->chunk);
+            if ($chunk > 0) {
+                $statement = $statement->limit($chunk);
             }
+
+            $records = $statement->process($this->csvReader);
         }
 
-        $records = $statement->process($csv);
-            $priceRepository = $this->em->getRepository(History::class);
-            foreach ($records as $key => $record) {
-                $instrument = $repository->findOneBySymbol($record['Symbol']);
-                $logMsg = sprintf('%s: ', $record['Symbol']);
-                $screenMsg = sprintf('%3.3d ', $key) . $logMsg;
-
-                if ($instrument) {
-                    // will only return price records which were marked for $this->provider name
-                    $history = $priceRepository->retrieveHistory($instrument, new \DateInterval($interval), $fromDate, $toDate, $this->provider);
-                    if (!empty($history)) {
-                        // backup existing csv file for the given period
-                        $exportFile = sprintf('%s/%s_%s.csv', $exportPath, $instrument->getSymbol(), $period);
-                        if ($this->filesystem->exists($exportFile)) {
-                            $backupFileName = $exportFile . '.bak';
-                            $this->filesystem->copy($exportFile, $backupFileName);
-                            $logMsg .= sprintf('backed up original file into %s', $backupFileName);
-                            $screenMsg .= 'backed_up ';
-                        }
-                        $csvWriter = Writer::createFromPath($exportFile, 'w');
-                        $header = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume'];
-                        $csvWriter->insertOne($header);
-                        $csvWriter->insertAll(array_map(function($v) {
-                            return [$v->getTimestamp()->format('Y-m-d'), $v->getOpen(), $v->getHigh(), $v->getLow(), $v->getClose(), $v->getVolume()];
-                        }, $history));
-
-                        $logMsg .= sprintf('exported PH %s ', $interval);
-                        $screenMsg .= sprintf('exported %s', $interval);
-                        unset($history);
-                    } else {
-                        $logMsg .= 'No price history is stored';
-                        $screenMsg .= 'no_PH ';
-                    }
-                } else {
-                    $logMsg .= sprintf('Could not find instrument=%s', $record['Symbol']);
-                    $screenMsg .=  'no_instr ';
-                }
-
-                $this->utilities->logAndSay($output, $logMsg, $screenMsg);
+        foreach ($records as $record) {
+            /** @var Instrument $instrument */
+            $instrument = $this->em->getRepository(Instrument::class)->findOneBySymbol($record['Symbol']);
+            $symbol = $instrument->getSymbol();
+            if (empty($instrument)) {
+                $output->writeln(
+                    '<warning>WARNING: </warning>Could not find instrument for symbol %s',
+                    $record['Symbol']
+                );
+                continue;
             }
+
+            $history = $this->em->getRepository(History::class)->retrieveHistory(
+                $instrument,
+                $interval,
+                $fromDate,
+                $toDate,
+                $provider
+            );
+            if (empty($history)) {
+                $output->writeln(sprintf(
+                    '<warning>WARNING: </warning> Could not find price history for symbol %s, interval = %s',
+                    $record['Symbol'],
+                    $intervalRaw
+                ));
+                continue;
+            }
+
+            $exportFileName = sprintf('%s_%s.csv', $symbol, $period);
+            $exportFile = $exportPath . '/' . $exportFileName;
+
+            if ($this->filesystem->exists($exportFile)) {
+                $backupFileName = $exportFile . '.bak';
+                $this->filesystem->copy($exportFile, $backupFileName);
+            }
+
+            $csvWriter = Writer::createFromPath($exportFile, 'w');
+            $header = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume'];
+            $csvWriter->insertOne($header);
+            $csvWriter->insertAll(array_map(function ($v) {
+                return [
+                  $v->getTimestamp()->format('Y-m-d'),
+                  $v->getOpen(),
+                  $v->getHigh(),
+                  $v->getLow(),
+                  $v->getClose(),
+                  $v->getVolume()
+                ];
+            }, $history));
+
+            unset($history);
+
+            $output->writeln(sprintf(
+                '%5.5s: Exported %s price history into file %s ',
+                $symbol,
+                $intervalRaw,
+                $exportFileName
+            ));
+        }
+
         $this->utilities->pronounceEnd($this, $output);
+
+        return 0;
     }
 }
